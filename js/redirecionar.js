e5adfd273a8392ccd189fa0e0a28b1eaf7437f27
Ângelo Artur <ang.vianna@gmail.com>
2023-08-26 20:23:52 -0300

    pequeno ajuste

--- js/redirecionar.js
+++ js/redirecionar.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loginButton = document.getElementById('loginButton');
     loginButton.addEventListener('click', function (event) {
-        event.preventDefault(); // Evita o comportamento padrão do botão
+        event.preventDefault();
         autenticar();
     });
 });
